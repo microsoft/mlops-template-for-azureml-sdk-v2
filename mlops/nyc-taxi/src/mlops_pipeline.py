eb1d51204e33e5986794394ce469f58138b8a3ed
Sergii Baidachnyi <sbaydach@microsoft.com>
2022-11-28 16:50:40 -0800

    one script execution

--- mlops/nyc-taxi/src/mlops_pipeline.py
+++ mlops/nyc-taxi/src/mlops_pipeline.py
@@ -5,55 +5,30 @@ from azure.ai.ml import MLClient, Input
 from azure.ai.ml import load_component
 import time
 import os
+from mlops.common import get_compute, get_environment
 
 
-parser = argparse.ArgumentParser("build_environment")
-parser.add_argument("--subscription_id", type=str, help="Azure subscription id")
-parser.add_argument("--resource_group_name", type=str, help="Azure Machine learning resource group")
-parser.add_argument("--workspace_name", type=str, help="Azure Machine learning Workspace name")
-parser.add_argument("--cluster_name", type=str, help="Azure Machine learning cluster name")
-parser.add_argument("--build_reference", type=str, help="Unique identifier for Azure DevOps pipeline run")
-parser.add_argument("--deploy_environment", type=str, help="execution and deployment environment. e.g. dev, prod, test")
-parser.add_argument("--experiment_name", type=str, help="Job execution experiment name")
-parser.add_argument("--ml_pipeline_name", type=str, help="name of pipeline")
-parser.add_argument("--wait_for_completion", type=str, help="determine if pipeline to wait for job completion")
-parser.add_argument("--environment_name", type=str, help="Azure Machine Learning Environment name for job execution")
+ml_pipeline_name = ""
+display_name = ""
+experiment_name = ""
+deploy_environment = ""
+build_reference = ""
 
-args = parser.parse_args()
-
-try:
-    client = MLClient(DefaultAzureCredential(),
-        subscription_id=args.subscription_id,
-        resource_group_name=args.resource_group_name,
-        workspace_name=args.workspace_name)
-except Exception as ex:
-   print("Oops!  invalid credentials.. Try again...")
-   raise
-
-parent_dir = os.path.join(os.getcwd(), "mlops/nyc-taxi/components")
-data_dir = os.path.join(os.getcwd(), "mlops/nyc-taxi/data/")
-
-prepare_data = load_component(source=parent_dir + "/prep.yml")
-transform_data = load_component(source=parent_dir + "/transform.yml")
-train_model = load_component(source=parent_dir + "/train.yml")
-predict_result = load_component(source=parent_dir + "/predict.yml")
-score_data = load_component(source=parent_dir + "/score.yml")
-
-# Set the environment name to custom environment using name and version number
-prepare_data.environment = f"azureml:{args.environment_name}@latest"
-transform_data.environment = f"azureml:{args.environment_name}@latest"
-train_model.environment = f"azureml:{args.environment_name}@latest"
-predict_result.environment = f"azureml:{args.environment_name}@latest"
-score_data.environment = f"azureml:{args.environment_name}@latest"
-
-@pipeline(name=args.ml_pipeline_name,
-    display_name=args.ml_pipeline_name, 
-    experiment_name=args.experiment_name, 
+@pipeline(name=ml_pipeline_name,
+    display_name=display_name, 
+    experiment_name=experiment_name, 
     tags={
-        'environment': args.deploy_environment,
-        'build_reference': args.build_reference 
+        'environment': deploy_environment,
+        'build_reference': build_reference 
     })
-def nyc_taxi_data_regression(pipeline_job_input):
+def nyc_taxi_data_regression(
+    pipeline_job_input,
+    prepare_data,
+    transform_data,
+    train_model,
+    predict_result,
+    score_data
+):
     prepare_sample_data = prepare_data(
         raw_data=pipeline_job_input,
         )
@@ -80,57 +55,146 @@ def nyc_taxi_data_regression(pipeline_job_input):
         "pipeline_job_score_report": score_with_sample_data.outputs.score_report
     }
 
+def construct_pipeline(
+    cluster_name: str,
+    environment_name: str
+):
+    parent_dir = os.path.join(os.getcwd(), "mlops/nyc-taxi/components")
+    data_dir = os.path.join(os.getcwd(), "mlops/nyc-taxi/data/")
 
-pipeline_job = nyc_taxi_data_regression(
-    Input(type="uri_folder", path=data_dir), 
-)
+    prepare_data = load_component(source=parent_dir + "/prep.yml")
+    transform_data = load_component(source=parent_dir + "/transform.yml")
+    train_model = load_component(source=parent_dir + "/train.yml")
+    predict_result = load_component(source=parent_dir + "/predict.yml")
+    score_data = load_component(source=parent_dir + "/score.yml")
 
-# demo how to change pipeline output settings
-pipeline_job.outputs.pipeline_job_prepped_data.mode = "rw_mount"
+    # Set the environment name to custom environment using name and version number
+    prepare_data.environment = environment_name
+    transform_data.environment = environment_name
+    train_model.environment = environment_name
+    predict_result.environment = environment_name
+    score_data.environment = environment_name
 
-# set pipeline level compute
-pipeline_job.settings.default_compute = args.cluster_name
+    pipeline_job = nyc_taxi_data_regression(
+        Input(type="uri_folder", path=data_dir),
+        prepare_data,
+        transform_data,
+        train_model,
+        predict_result,
+        score_data
+    )
 
-# set pipeline level datastore
-pipeline_job.settings.default_datastore = "workspaceblobstore"
+    # demo how to change pipeline output settings
+    pipeline_job.outputs.pipeline_job_prepped_data.mode = "rw_mount"
 
-# Submit pipeline job to workspace
-pipeline_job = client.jobs.create_or_update(
-    pipeline_job, experiment_name=args.experiment_name 
-)
+    # set pipeline level compute
+    pipeline_job.settings.default_compute = cluster_name
 
-if pipeline_job == None:
-    print("Job creation failed")
-    raise
+    # set pipeline level datastore
+    pipeline_job.settings.default_datastore = "workspaceblobstore"
 
-current_job = client.jobs.get(pipeline_job.name)
-if current_job == None:
-    print("no job found with given name")
-    raise
+    return pipeline_job
 
-if args.wait_for_completion == "True":
-    total_wait_time = 1800
-    current_wait_time = 0
-    job_status = ["NotStarted", "Queued", "Starting", "Preparing", "Running", "Finalizing"]
-    while current_job.status in job_status:
-        if current_wait_time <= total_wait_time:
-            time.sleep(15)
-            current_job = client.jobs.get(pipeline_job.name) 
-            print(current_job.status)
-            current_wait_time += current_wait_time + 15
+def execute_pipeline(
+    subscription_id: str,
+    resource_group_name: str,
+    workspace_name: str,
+    experiment_name: str,
+    pipeline_job: pipeline,
+    wait_for_completion: str
+):
+    try:
+        client = MLClient(
+            DefaultAzureCredential(),
+            subscription_id=subscription_id,
+            resource_group_name=resource_group_name,
+            workspace_name=workspace_name)
+
+        pipeline_job = client.jobs.create_or_update(
+            pipeline_job, experiment_name=experiment_name 
+        )
+    
+        if wait_for_completion == "True":
+            job_status = ["NotStarted", "Queued", "Starting", "Preparing", "Running", "Finalizing"]
+            while pipeline_job.status in job_status:
+                time.sleep(15)
+                pipeline_job = client.jobs.get(pipeline_job.name) 
+                print(pipeline_job.status)
+        if pipeline_job.status == 'Completed' or pipeline_job.status == 'Finished':
+            print(f"job status: {pipeline_job.status}")
+            print("exiting job successfully..")
         else:
-            break
-    print(current_job.status)
-    if current_job.status == 'Completed' or current_job.status == 'Finished':
-        print(f"job status: {current_job.status}")
-        print("exiting job successfully..")
-    else:
-        print("exiting job with failure..")
-     
+            print("exiting job with failure..")
+    except Exception as ex:
+        print("Oops! invalid credentials or error while creating ML environment.. Try again...")
+        raise
 
 
+def main():
+    parser = argparse.ArgumentParser("build_environment")
+    parser.add_argument("--subscription_id", type=str, help="Azure subscription id")
+    parser.add_argument("--resource_group_name", type=str, help="Azure Machine learning resource group")
+    parser.add_argument("--workspace_name", type=str, help="Azure Machine learning Workspace name")
+    parser.add_argument("--cluster_name", type=str, help="Azure Machine learning cluster name")
+    parser.add_argument("--cluster_size", type=str, help="Azure Machine learning cluster size")
+    parser.add_argument("--cluster_region", type=str, help="Azure Machine learning cluster region")
+    parser.add_argument("--min_instances", type=int, default=0)
+    parser.add_argument("--max_instances", type=int, default=4)
+    parser.add_argument("--idle_time_before_scale_down", type=int, default=120)
+    parser.add_argument("--build_reference", type=str, help="Unique identifier for Azure DevOps pipeline run")
+    parser.add_argument("--deploy_environment", type=str, help="execution and deployment environment. e.g. dev, prod, test")
+    parser.add_argument("--experiment_name", type=str, help="Job execution experiment name")
+    parser.add_argument("--display_name", type=str, help="Job execution run name")
+    parser.add_argument("--ml_pipeline_name", type=str, help="name of pipeline")
+    parser.add_argument("--wait_for_completion", type=str, help="determine if pipeline to wait for job completion")
+    parser.add_argument("--environment_name", type=str, help="Azure Machine Learning Environment name for job execution")
+    parser.add_argument("--env_base_image_name", type=str, help="Environment custom base image name")
+    parser.add_argument("--conda_path", type=str, help="path to conda requirements file")
+    parser.add_argument("--env_description", type=str, default="Environment created using Conda.")
+
+    args = parser.parse_args()
+
+    compute = get_compute(
+        args.subscription_id,
+        args.resource_group_name,
+        args.workspace_name,
+        args.cluster_name,
+        args.cluster_size,
+        args.cluster_region,
+        args.min_instances,
+        args.max_instances,
+        args.idle_time_before_scale_down
+    )
+
+    environment = get_environment(
+        args.subscription_id,
+        args.resource_group_name,
+        args.workspace_name,
+        args.env_base_image_name,
+        args.conda_path,
+        args.environment_name,
+        args.env_description
+    )
+
+    ml_pipeline_name = args.ml_pipeline_name
+    display_name = args.display_name
+    experiment_name = args.experiment_name
+    deploy_environment = args.deploy_environment
+    build_reference = args.build_reference
+
+    pipeline_job = construct_pipeline(
+        compute.name,
+        environment.name
+    )
+
+    execute_pipeline(
+        args.subscription_id,
+        args.resource_group_name,
+        args.workspace_name,
+        args.experiment_name,
+        pipeline_job,
+        args.wait_for_completion)
 
 
-
-
-
+if __name__ == "__main__":
+    main()
