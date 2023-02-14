f521ce7e093feb49f89ca07901a48ec139e2f2db
Ritesh Modi <rimod@microsoft.com>
2023-02-14 17:23:13 +0000

    change in dockerfile

--- src/nyc_src/train/train.py
+++ src/nyc_src/train/train.py
@@ -95,7 +95,7 @@ def train_model(trainX, trainy):
         #pickle.dump(model, open((Path(args.model_output) / "model.sav"), "wb"))
         mlflow.sklearn.save_model(sk_model=model,path=args.model_output)
         print(mlflow.active_run().info.run_id)
-        mlflow.register_model("runs:/" + mlflow.active_run().info.run_id + "/" + args.model_output)
+        mlflow.register_model("runs:/" + mlflow.active_run().info.run_id + "/" + args.model_output, "dummy_model")
         #model_info = mlflow.sklearn.log_model(model, model_output)
         #mlflow.register_model(model_info.model_uri, "mymodel1")
 
